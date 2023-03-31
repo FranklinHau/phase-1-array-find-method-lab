60737bf01fc49807a0acb42a871cadab2e36765d
Franklin Hau <fhauchi@gmail.com>
2023-03-31 14:42:21 -0600

    Array-Find-Method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"}
+    
+  ]
+
+    const superbowlWin = (record) => {
+      const foundResult = record.find(element => element.result === "W")
+        return foundResult ? foundResult.year : undefined;
+      }
+      console.log(superbowlWin(record))
+  
